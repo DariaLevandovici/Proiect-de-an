741f23da9c0102136f4f54c7a4995e7cae59010b
Levandovici Daria <daria.levandovici@isa.utm.md>
2026-04-08 12:18:02 +0300

    am adaugat fisier pentru importul imaginilor

---
+++ my-react-app/src/assets/MenuImages.ts
@@ -0,0 +1,81 @@
+import AvocadoToast from "./Menu/AvocadoToast.jpg";
+import BeefCarpaccio from "./Menu/BeefCarpaccio.jpg";
+import BruschettaClassica from "./Menu/BruschettaClassica.jpg";
+import BuddhaBowl from "./Menu/BuddhaBowl.jpg";
+import CaesarSalad from "./Menu/CaesarSalad.jpg";
+import CalamariFritti from "./Menu/CalamariFritti.jpg";
+import CapreseSalad from "./Menu/CapreseSalad.jpg";
+import Cheesecake from "./Menu/Cheesecake.jpg";
+import ChickenParmesan from "./Menu/ChickenParmesan.jpg";
+import ChocolateFondant from "./Menu/ChocolateFondant.jpg";
+import CraftBeer from "./Menu/CraftBeer.jpg";
+import CremeBrulee from "./Menu/CremeBrulee.jpg";
+import DuckBreast from "./Menu/DuckBreast.jpg";
+import EggsBenedict from "./Menu/EggsBenedict.jpg";
+import EnglishBreakfast from "./Menu/EnglishBreakfast.jpg";
+import Espresso from "./Menu/Espresso.jpg";
+import FrenchToast from "./Menu/FrenchToast.jpg";
+import FreshOrangeJuice from "./Menu/FreshOrangeJuice.jpg";
+import GelatoSelection from "./Menu/GelatoSelection.jpg";
+import GrilledSalmon from "./Menu/GrilledSalmon.jpg";
+import HouseWine from "./Menu/HouseWine.jpg";
+import HummusPlatter from "./Menu/HummusPlatter.jpg";
+import IcedMatchaLatte from "./Menu/IcedMatchaLatte.jpg";
+import LobsterLinguine from "./Menu/LobsterLinguine.jpg";
+import MojitoMocktail from "./Menu/MojitoMocktail.jpg";
+import MushroomRisotto from "./Menu/MushroomRisotto.jpg";
+import PancakeStack from "./Menu/PancakeStack.jpg";
+import PannaCotta from "./Menu/PannaCotta.jpg";
+import RackOfLamb from "./Menu/Rackoflamb.jpg";
+import RibeyeSteak from "./Menu/RibeyeSteak.jpg";
+import SeafoodPaella from "./Menu/SeafoodPaella.jpg";
+import SmoothieBowl from "./Menu/SmoothieBowl.jpg";
+import SpaghettiCarbonara from "./Menu/SpaghettiCarbonara.jpg";
+import SparklingWater from "./Menu/SparklingWater.jpg";
+import Tiramisu from "./Menu/Tiramisu.jpg";
+import VeganTacos from "./Menu/VeganTacos.jpg";
+import VeganBrownie from "./Menu/VeganBrownie.jpg";
+import VeganBurger from "./Menu/VeganBurger.jpg";
+import ZucchiniNoodles from "./Menu/ZucchiniNoodles.jpg";
+
+export const menuImages = {
+  AvocadoToast,
+  BeefCarpaccio,
+  BruschettaClassica,
+  BuddhaBowl,
+  CaesarSalad,
+  CalamariFritti,
+  CapreseSalad,
+  Cheesecake,
+  ChickenParmesan,
+  ChocolateFondant,
+  CraftBeer,
+  CremeBrulee,
+  DuckBreast,
+  EggsBenedict,
+  EnglishBreakfast,
+  Espresso,
+  FrenchToast,
+  FreshOrangeJuice,
+  GelatoSelection,
+  GrilledSalmon,
+  HouseWine,
+  HummusPlatter,
+  IcedMatchaLatte,
+  LobsterLinguine,
+  MojitoMocktail,
+  MushroomRisotto,
+  PancakeStack,
+  PannaCotta,
+  RackOfLamb,
+  RibeyeSteak,
+  SeafoodPaella,
+  SmoothieBowl,
+  SpaghettiCarbonara,
+  SparklingWater,
+  Tiramisu,
+  VeganTacos,
+  VeganBrownie,
+  VeganBurger,
+  ZucchiniNoodles,
+};
