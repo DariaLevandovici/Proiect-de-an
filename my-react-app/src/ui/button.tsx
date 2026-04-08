43d59f0a2c8352e67ec0a4bd111add48084a6d49
Levandovici Daria <daria.levandovici@isa.utm.md>
2026-04-08 16:26:38 +0300

    am modificat stilul butonului

--- my-react-app/src/ui/button.tsx
+++ my-react-app/src/ui/button.tsx
@@ -10,6 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-blue-700 text-white hover:bg-blue-600 shadow-sm hover:shadow-md",
+        success: "bg-green-700 text-white hover:bg-green-600 shadow-sm hover:shadow-md",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
