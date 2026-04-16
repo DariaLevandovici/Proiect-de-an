ee33e4c6e713866cdbeef39e2ce42da0620186f3
danielrusu0702 <rusu66970@gmail.com>
2026-04-16 10:17:46 +0300

    feat: scroll automat sus la schimbarea paginii

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,6 +1,16 @@
-import { RouterProvider } from 'react-router';
+import { Outlet, RouterProvider } from 'react-router';
 import { router } from './routes';
 import { AppProvider } from './context/AppContext';
+import { ScrollToTop } from './components/ScrollToTop';
+
+export function AppLayout() {
+  return (
+    <>
+      <ScrollToTop />
+      <Outlet />
+    </>
+  );
+}
 
 export default function App() {
   return (
